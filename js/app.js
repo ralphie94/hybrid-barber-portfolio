9ac75df6f81533772de3edc1b3eb4064b608df3a
Ralphie Celedon <ralphie94@pacbell.net>
2023-04-07 10:41:26 -0700

    created custom review slider in testimonials

--- js/app.js
+++ js/app.js
@@ -35,3 +35,16 @@ const navSlide = () => {
 }
 
 navSlide();
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
